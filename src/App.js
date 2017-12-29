a93c7bf4c236d807d6362d0b2415d24cfb4b8482
rahul4times <tkdmaster.sharma@gmail.com>
2017-12-29 16:31:49 -0700

    top gainers added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
+
+  state = {
       response: false,
-      endpoint: "http://127.0.0.1:4000"
-    };
-  }
+      endpoint: "http://127.0.0.1:4000",
+      gainers: false
+    }
+
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
     socket.on("FromAPI", data => this.setState({ response: data }));
+    socket.on("topGainerList", data => this.setState({ gainers: data }));
+
   }
+
   render() {
     const { response } = this.state;
+
+    const topGainers = this.state.gainers ? this.state.gainers.map(gainer => {
+      return(
+              <tr key={gainer.symbol}>
+                <td>
+                  {gainer.symbol}
+                </td>
+                <td>
+                  {gainer.companyName}
+                </td>
+              </tr>
+      );
+    }) : "Loading...";
+
+
     return (
       <div style={{ textAlign: "center" }}>
         {response
           ? <p>
-              Latest Price : {response}
+              Latest Price : {response.latestPrice}
             </p>
-          : <p>Loading...!</p>}
+          : <p>Loading...</p>}
+        <br/>
+        <h3>Top Gainers</h3>
+        <table>
+          <tr>
+            <th>Symbol</th>
+            <th>Company Name</th>
+          </tr>
+
+            {topGainers}
+          
+        </table>
       </div>
     );
   }
